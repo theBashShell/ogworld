398bf35e6165a80879803e094944019f45209f41
theBashShell <edohbruno@gmail.com>
2019-10-13 01:42:04 +0000

    server works

--- src/server/server.js
+++ src/server/server.js
@@ -1,8 +1,38 @@
 const app = require('express')();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const nodemailer = require('nodemailer');
 
 const PORT = process.env.PORT || 9090;
+const HOSTNAME = 'ogworldbackend.herokuapp.com';
+const RECEIVER_EMAIL = 'test@ogworldgh.com';
+const HOST = 'uk7.fcomet.com'
+
+
+
+
+
+const sendMail = async (name, email, message) => {
+  let transporter = nodemailer.createTransport({
+    host: HOST,
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.OG_USER,
+      pass: process.env.OG_PASS,
+    },
+  });
+
+  let info = await transporter.sendMail({
+    from: `${name} <${email}>`,
+    to: RECEIVER_EMAIL,
+    subject: `Message from ${name} ✔`,
+    text: `${message}`,
+    html: `<p>${message}</p>`,
+  });
+
+  console.log('Message sent: %s', info.messageId);
+};
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -12,8 +42,8 @@ app.get('/', (req, res) => res.send('working'));
 
 app.post('/', (req, res) => {
   console.log(req.hostname);
-  //    res.send(req)
   console.log(req.body);
+  sendMail(req.body.name, req.body.email, req.body.message);
 });
 
 app.listen(PORT, a => console.log('app running on http://localhost:', PORT));
