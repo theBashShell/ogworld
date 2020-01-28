03b0f1ddd1e6fc0afae832020c385c04288ac5be
theBashShell <edohbruno@gmail.com>
2020-01-28 19:38:31 +0000

    clash

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Helmet from "../components/meta/Helmet";
+import {PrimaryButton} from 'office-ui-fabric-react'
 
-const index = () => {
+const index = () => { 
   return (
     <div>
       <Helmet>
         <title>OG World | Opening Ghana to the World</title>
       </Helmet>
-      Home
+      Hom
     </div>
   );
 };
