5e85127302a683505639973a7975d0dd67275095
theBashShell <edohbruno@gmail.com>
2020-02-03 23:09:06 +0000

    services done

--- src/components/og/OGServices.jsx
+++ src/components/og/OGServices.jsx
@@ -6,42 +6,44 @@ import OGService from "./OGService";
 const OGServices = () => {
   return (
     <section className="services centered">
-      <div className="sec-title">Services</div>
-      <OGService
-        pic={CONSTANTS.images.exchange_program}
-        color="#ffb0b0"
-        head="Exchange Program"
-        text="OG World connects institutions willing to give
+      <div className="sec-title s-sec-title">Services</div>
+      <div className="centered contained">
+        <OGService
+          pic={CONSTANTS.images.exchange_program}
+          color="#ffb0b0"
+          head="Exchange Program"
+          text="OG World connects institutions willing to give
            opportunity to young Ghanaians to develop their 
            talent and return to their communities to help make
             a difference. OG world depends on its partners to 
             achieve the objectives of this program."
-      />
-      <OGService
-        pic={CONSTANTS.images.logistics}
-        color="#ffedb0"
-        head="Logisitics & Accomodation"
-        text="OG WORLD provides in-country logistics primarily 
+        />
+        <OGService
+          pic={CONSTANTS.images.logistics}
+          color="#ffedb0"
+          head="Logisitics & Accomodation"
+          text="OG WORLD provides in-country logistics primarily 
           for its guests, partners and partner organizations 
           including, internal travel arrangements, setting up 
           of meetings, visits to places of interest in Ghana as
            well as community needs assessment for Project development."
-      />
-      <OGService
-        pic={CONSTANTS.images.visa}
-        color="#a5fec0"
-        head="Visa & Ticket"
-        text="OG World facilitates visas and tickets for people visiting 
+        />
+        <OGService
+          pic={CONSTANTS.images.visa}
+          color="#a5fec0"
+          head="Visa & Ticket"
+          text="OG World facilitates visas and tickets for people visiting 
           Ghana. Experience and explore Ghana while OG World 
           does the heavy lifting."
-      />
-      <OGService
-        pic={CONSTANTS.images.more}
-        color="#fde2f7"
-        head="More..."
-        text="Planning on visiting Ghana and have a memorable experience? 
+        />
+        <OGService
+          pic={CONSTANTS.images.more}
+          color="#fde2f7"
+          head="More..."
+          text="Planning on visiting Ghana and have a memorable experience? 
         Speak to us."
-      />
+        />
+      </div>
     </section>
   );
 };
