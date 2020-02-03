9ab7de49bc8697a8246a47b6b1800eadd85ce084
theBashShell <edohbruno@gmail.com>
2020-02-03 22:39:14 +0000

    lazy image for services

--- src/components/og/OGService.jsx
+++ src/components/og/OGService.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./ogservice.css";
+import "lazysizes";
 
 const OGService = ({ pic, head, text, color }) => {
   return (
     <div className="s-container" style={{ background: color }}>
       <div className="s-img-container">
-        <img src={pic} alt={head} />
+        <img data-src={pic} alt={head} className="lazyload" />
       </div>
       <h3 className="s-head">{head}</h3>
       <div className="s-text">{text}</div>
