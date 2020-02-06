c02b21cdc34ee1fa7e40ef762ee07b4ff844fa85
theBashShell <edohbruno@gmail.com>
2020-02-06 22:55:55 +0000

    gallery connected

--- src/components/og/OGGallery.jsx
+++ src/components/og/OGGallery.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import "./oggallery.css"
+import React from "react";
+import "./oggallery.css";
+import CONSTANTS from '../../util/constants'
 import Gallery from "react-grid-gallery";
 
 const OGGallery = () => {
   return (
     <section className="gallery centered">
       gallery
+      <Gallery images={CONSTANTS.gallery} />
     </section>
   );
-}
+};
 
 export default OGGallery;
