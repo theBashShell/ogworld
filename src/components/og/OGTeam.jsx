f32e6470f5c47eab5af75771d4d8867b2c22b045
theBashShell <edohbruno@gmail.com>
2020-02-08 14:58:24 +0000

    team fixed

--- src/components/og/OGTeam.jsx
+++ src/components/og/OGTeam.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import "./ogteam.css"; 
+import "./ogteam.css";
 import CONSTANTS from "../../util/constants";
 
 const Member = ({ pic, head, desc }) => (
@@ -12,9 +12,9 @@ const Member = ({ pic, head, desc }) => (
   </div>
 );
 
-const OGTeam = () => { 
+const OGTeam = () => {
   return (
-    <section id='team' className="team">
+    <section id="team" className="team">
       <div class="sec-title team-title">Team</div>
       <div class="centered whole-team">
         <Member
@@ -26,7 +26,7 @@ const OGTeam = () => {
            He is passionate about giving the needed exposure to the
             youth through interactions with other persons from the 
             diaspora. Edward is always determined to achieve success 
-            in all his endevors"
+            in all his endevors."
         />
         <Member
           pic={CONSTANTS.images.vero}
@@ -34,7 +34,7 @@ const OGTeam = () => {
           desc="Veronica has great passion for reaching out to the
            less privileged. She has 9 years of experience in resource
             management. Her high sense of integrity and commitment with 
-            which she takes on tasks are excellent. She loves to swim and read"
+            which she takes on tasks are excellent. She loves to swim and read."
         />
         <Member
           pic={CONSTANTS.images.bruno}
@@ -43,7 +43,7 @@ const OGTeam = () => {
         is a software engineer currently working with OG World. He has a B.Sc. 
         in Computer Science from Ashesi University. He is interested in building
          productivity tools and enjoys technical writing. In his spare time, he 
-         is tinkering with new technology"
+         is tinkering with new technology."
         />
       </div>
       <div className="swiper-pg" />
