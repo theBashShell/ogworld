1ddfb05586ace33f92df52c50243bcd81fb0c435
theBashShell <edohbruno@gmail.com>
2020-02-08 14:36:37 +0000

    fixed header slide

--- src/components/index/IndexTop.jsx
+++ src/components/index/IndexTop.jsx
@@ -1,10 +1,19 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect } from "react";
 import "lazysizes";
 import "./indextop.css";
 import Swiper from "swiper";
-import "lazysizes";
 import CONSTANTS from "../../util/constants";
 
+const SlidePic = ({ pic, thumbnail }) => (
+  <div className="swiper-slide">
+    <img
+      data-src={pic}
+      alt="background"
+      className="bg-img lazyload"
+      src={thumbnail}
+    />
+  </div>
+);
 const IndexTop = () => {
   useEffect(() => {
     new Swiper(".top", {
@@ -15,7 +24,6 @@ const IndexTop = () => {
         el: ".swiper-pagination",
         clickable: true
       },
-      loop: true,
       keyboard: {
         enabled: true
       },
@@ -26,35 +34,15 @@ const IndexTop = () => {
   }, []);
 
   return (
-    <div id='top' className=" swiper-container top">
+    <div id="top" className=" swiper-container top">
       <div className="swiper-wrapper">
         <div className="top-info">
           <div className="slogan">Opening Ghana to the World</div>
           <img className="scroll" src={CONSTANTS.images.scroll} alt="scroll" />
         </div>
-        <div className="swiper-slide">
-          <img
-            data-src={CONSTANTS.images.independence_square}
-            alt="background image"
-            class="bg-img lazyload"
-            src={CONSTANTS.images.independenc_square_thumbnail}
-          />
-        </div>
-        <div className="swiper-slide">
-          <img
-            data-src={CONSTANTS.images.beach}
-            alt="background image"
-            class="bg-img lazyload"
-          />
-        </div>
-        <div className="swiper-slide">
-          <img
-            data-src={CONSTANTS.images.craft}
-            alt="background image"
-            class="bg-img lazyload"
-            src={CONSTANTS.images.craft_thumbnail}
-          />
-        </div>
+        {CONSTANTS.pageslide.map(el => (
+          <SlidePic {...el} key={el.pic} />
+        ))}
       </div>
       <div className="swiper-pagination"></div>
     </div>
