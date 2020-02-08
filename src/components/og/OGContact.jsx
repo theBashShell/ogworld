f9ef26b89f4174b4e89a2a533b516285407e4cb7
theBashShell <edohbruno@gmail.com>
2020-02-08 12:41:06 +0000

    form

--- src/components/og/OGContact.jsx
+++ src/components/og/OGContact.jsx
@@ -9,7 +9,7 @@ const ContactForm = () => {
   const formik = useFormik({
     initialValues: {
       name: "",
-      email: "example@example.com",
+      email: "",
       message: "",
       onSubmit: async values => {
         console.log(values);
@@ -17,17 +17,36 @@ const ContactForm = () => {
     }
   });
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <label htmlFor="name">Name</label>
-      <TextField name="name" width="large" />
+    <form onSubmit={formik.handleSubmit} className="ct-form"> 
+        <label htmlFor="name">Name</label>
+        <TextField
+          name="name"
+          className="ct-input"
+          placeholder="Jane Doe"
+          {...formik.getFieldProps("name")}
+        />
 
-      <label htmlFor="email">Email</label>
-      <TextField name="email" width="large" type="email" />
+        <label htmlFor="email">Email</label>
+        <TextField
+          name="email"
+          type="email"
+          className="ct-input"
+          placeholder="example@example.com"
+          {...formik.getFieldProps("email")}
+        />
 
-      <label htmlFor="message">Message</label>
-      <TextArea name="message" width="large" value="" resize="vertical" />
-      
-      <Button type='submit' appearance='primary'>Submit</Button>
+        <label htmlFor="message">Message</label>
+        <TextArea
+          name="message"
+          value=""
+          resize="vertical"
+          placeholder="What's on your mind?"
+          {...formik.getFieldProps("message")}
+        />
+
+        <Button type="submit" appearance="primary">
+          Submit
+        </Button> 
     </form>
   );
 };
@@ -35,8 +54,8 @@ const ContactForm = () => {
 const OGContact = () => {
   return (
     <section className="contact centered">
-      contact
-      <div>
+      <div class="sec-title ct-title">Contact Us</div>
+      <div >
         <ContactForm />
       </div>
     </section>
