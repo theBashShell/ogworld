dec11988ad59fe16a54edc10ba2ee42db94b907b
theBashShell <edohbruno@gmail.com>
2020-02-09 23:05:40 +0000

    backend start

---
+++ api/mailer.js
@@ -0,0 +1,31 @@
+const nodemailer = require("nodemailer");
+
+module.exports = (req, res) => {
+  const { name = "World" } = req.query;
+  res.send(`Hello ${name}!`);
+};
+
+const CONFIG = {
+  host: process.env.OG_HOST,
+  port: process.env.OG_PORT,
+  secure: true,
+  auth: {
+    user: process.env.OG_USER,
+    pass: process.env.OG_SECRET
+  }
+};
+
+const emailUser = async ({ file, name, email }) => {
+  const transporter = nodemailer.createTransport({ ...CONFIG });
+
+  let info = await transporter.sendMail({
+    from: `"Fund Request: ${name} 👻" <fundus.flask@gmail.com>`,
+    to: `${process.env.RQST_RECEIVER}`,
+    subject: "Fund Request ✔",
+    text: `New Request attached: from ---${name} -- ${email}`,
+    html: `<p>New Request attached: from ---${name} -- ${email}</p>`,
+    attachments: [{ filename: "Fund Request", path: file }],
+    cc: [email, `${process.env.RQST_USER}`]
+  });
+  console.log(info);
+};
